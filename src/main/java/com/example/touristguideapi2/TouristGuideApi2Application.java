ca341434c3bc01111312aa36b3b24d3634baf5b0
saal0001 <saal0001@stud.kea.dk>
2024-02-11 14:00:16 +0100

    Initial commit

---
+++ src/main/java/com/example/touristguideapi2/TouristGuideApi2Application.java
@@ -0,0 +1,13 @@
+package com.example.touristguideapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuideApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristGuideApi2Application.class, args);
+    }
+
+}
